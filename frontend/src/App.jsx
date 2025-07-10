9f45a0ec843bdf91c0cb6dc4168f2f5d14de5ed9
Virendra69 <panchalvir12@gmail.com>
2025-07-10 10:52:30 +0530

    Created routes in the App to navigate to different pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import MeetingTable from "./components/MeetingTable";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MinutesChatPage from "./components/MinutesChatPage";
 
 function App() {
   return (
     <div className="h-screen flex">
-      <MeetingTable />
+      <Router>
+        <Routes>
+          <Route path="/" element={<MeetingTable />} />
+          <Route path="/minutes-chat" element={<MinutesChatPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
